3cd7034dc7dd8d75b3a5055fa50e4ebdf7f7b7a0
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-11-30 15:13:12 +0100

    chore: wait for postgres container

--- lib/utils/test_utils.go
+++ lib/utils/test_utils.go
@@ -2,10 +2,10 @@ package utils
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"time"
 
+	"github.com/docker/go-connections/nat"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
 )
@@ -25,50 +25,6 @@ type TestContainerConfiguration struct {
 	Database  string
 }
 
-func waitForPostgresQuery(ctx context.Context, cfg *TestContainerConfiguration, query string, timeout time.Duration) error {
-	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.Database)
-
-	ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
-
-	ticker := time.NewTicker(500 * time.Millisecond)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return fmt.Errorf("timeout waiting for postgres query: %w", ctx.Err())
-		case <-ticker.C:
-			db, err := sql.Open("postgres", dsn)
-			if err != nil {
-				// kurz warten und erneut versuchen
-				continue
-			}
-			// Limitieren der Verbindungen für schnelle Open/Close Versuche
-			db.SetConnMaxLifetime(2 * time.Second)
-			db.SetMaxOpenConns(1)
-			db.SetMaxIdleConns(0)
-
-			// optional: Ping zuerst prüfen
-			if err = db.PingContext(ctx); err != nil {
-				_ = db.Close()
-				continue
-			}
-
-			// Query ausführen (erwartet z.B. eine int-Spalte wie bei SELECT 1)
-			var tmp interface{}
-			if err = db.QueryRowContext(ctx, query).Scan(&tmp); err != nil {
-				_ = db.Close()
-				continue
-			}
-
-			_ = db.Close()
-			return nil
-		}
-	}
-}
-
 func PreparePostgresDB() (*TestContainerConfiguration, error) {
 	var env = map[string]string{
 		"POSTGRES_PASSWORD": DbPass,
@@ -80,7 +36,11 @@ func PreparePostgresDB() (*TestContainerConfiguration, error) {
 		ctx, "postgres:alpine",
 		testcontainers.WithExposedPorts("5432/tcp"),
 		testcontainers.WithWaitStrategy(
-			wait.ForListeningPort("5432/tcp"),
+			wait.ForSQL("5432/tcp", "postgres", func(host string, port nat.Port) string {
+				return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", DbUser, DbPass, host, port.Port(), DbName)
+			}).
+				WithStartupTimeout(time.Second*5).
+				WithQuery("SELECT 10"),
 		),
 		testcontainers.WithEnv(env),
 	)
@@ -107,10 +67,6 @@ func PreparePostgresDB() (*TestContainerConfiguration, error) {
 		Username:  DbUser,
 		Password:  DbPass,
 		Database:  DbName,
-	}
-
-	if err := waitForPostgresQuery(ctx, &tcfg, "SELECT 1", 30*time.Second); err != nil {
-		return nil, err
 	}
 
 	return &tcfg, nil
