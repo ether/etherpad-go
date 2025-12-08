77d8cd23aac1a78b8b8c757c54ba72de756a8d01
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-12-08 22:40:57 +0100

    chore: added testing for mysql

--- lib/db/MySQLDB.go
+++ lib/db/MySQLDB.go
@@ -891,57 +891,53 @@ func NewMySQLDB(options MySQLOptions) (*MysqlDB, error) {
 	if err != nil {
 		return nil, err
 	}
-	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS pad (id TEXT PRIMARY KEY, data TEXT)")
 
-	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS globalAuthor(id TEXT PRIMARY KEY, colorId TEXT, name TEXT, timestamp BIGINT)")
+	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS pad (id VARCHAR(255) PRIMARY KEY, data TEXT)")
 	if err != nil {
 		return nil, err
 	}
 
-	if err != nil {
-		return nil, err
-	}
-	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS padRev(id TEXT, rev INTEGER, changeset TEXT, atextText TEXT, atextAttribs TEXT, authorId TEXT, timestamp BIGINT, PRIMARY KEY (id, rev), FOREIGN KEY(id) REFERENCES pad(id) ON DELETE CASCADE)")
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS token2author(token TEXT PRIMARY KEY, author TEXT, FOREIGN KEY(author) REFERENCES globalAuthor(id) ON DELETE CASCADE)")
-
+	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS globalAuthor(id VARCHAR(255) PRIMARY KEY, colorId VARCHAR(50), name VARCHAR(255), timestamp BIGINT)")
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS globalAuthorPads(id TEXT NOT NULL, padID TEXT NOT NULL,  PRIMARY KEY(id, padID), FOREIGN KEY(id) REFERENCES globalAuthor(id) ON DELETE CASCADE, FOREIGN KEY(padID) REFERENCES pad(id) ON DELETE CASCADE)")
-
+	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS padRev(id VARCHAR(255), rev INTEGER, changeset TEXT, atextText TEXT, atextAttribs TEXT, authorId VARCHAR(255), timestamp BIGINT, PRIMARY KEY (id, rev), FOREIGN KEY(id) REFERENCES pad(id) ON DELETE CASCADE)")
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS pad2readonly(id TEXT PRIMARY KEY, data TEXT, FOREIGN KEY(id) REFERENCES pad(id) ON DELETE CASCADE)")
-	if err != nil {
-		return nil, err
-	}
-	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS readonly2pad(id TEXT PRIMARY KEY, data TEXT, FOREIGN KEY(data) REFERENCES pad(id) ON DELETE CASCADE)")
-
+	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS token2author(token VARCHAR(255) PRIMARY KEY, author VARCHAR(255), FOREIGN KEY(author) REFERENCES globalAuthor(id) ON DELETE CASCADE)")
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS sessionstorage(id TEXT PRIMARY KEY, originalMaxAge INTEGER, expires TEXT, secure BOOLEAN, httpOnly BOOLEAN, path TEXT, sameSite TEXT, connections TEXT)")
-
+	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS globalAuthorPads(id VARCHAR(255) NOT NULL, padID VARCHAR(255) NOT NULL, PRIMARY KEY(id, padID), FOREIGN KEY(id) REFERENCES globalAuthor(id) ON DELETE CASCADE, FOREIGN KEY(padID) REFERENCES pad(id) ON DELETE CASCADE)")
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS groupPadGroup(id TEXT PRIMARY KEY, name TEXT)")
-
+	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS pad2readonly(id VARCHAR(255) PRIMARY KEY, data VARCHAR(255), FOREIGN KEY(id) REFERENCES pad(id) ON DELETE CASCADE)")
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS padChat(padId TEXT NOT NULL, padHead INTEGER,  chatText TEXT NOT NULL, authorId TEXT, timestamp BIGINT, PRIMARY KEY(padId, padHead), FOREIGN KEY(padId) REFERENCES pad(id) ON DELETE CASCADE)")
+	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS readonly2pad(id VARCHAR(255) PRIMARY KEY, data VARCHAR(255), FOREIGN KEY(data) REFERENCES pad(id) ON DELETE CASCADE)")
+	if err != nil {
+		return nil, err
+	}
 
+	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS sessionstorage(id VARCHAR(255) PRIMARY KEY, originalMaxAge INTEGER, expires VARCHAR(255), secure BOOLEAN, httpOnly BOOLEAN, path VARCHAR(255), sameSite VARCHAR(50), connections TEXT)")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS groupPadGroup(id VARCHAR(255) PRIMARY KEY, name VARCHAR(255))")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = sqlDb.Exec("CREATE TABLE IF NOT EXISTS padChat(padId VARCHAR(255) NOT NULL, padHead INTEGER, chatText TEXT NOT NULL, authorId VARCHAR(255), timestamp BIGINT, PRIMARY KEY(padId, padHead), FOREIGN KEY(padId) REFERENCES pad(id) ON DELETE CASCADE)")
 	if err != nil {
 		return nil, err
 	}
