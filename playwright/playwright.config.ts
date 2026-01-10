62d6413d9620c13dbb787f16147cd5454932d6b0
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2026-01-10 22:15:58 +0100

    chore: do playwright tests

---
+++ playwright/playwright.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: '.',
+  testMatch: ['**/*.spec.ts'],
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI ? [['html', { open: 'never' }], ['github']] : 'html',
+  use: {
+    baseURL: 'http://localhost:9001',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+  webServer: {
+    command: 'cd .. && ./etherpad-go',
+    url: 'http://localhost:9001',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+  },
+});
+
